40f054c38c9c3471731153987c3a2d975ae3181f
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-08 13:23:54 -0800

    styling changes

--- console.py
+++ console.py
@@ -1,24 +1,28 @@
+#!/usr/bin/python3
+# Script that implements a custom CLI for AirBnB project
+
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """A simple Air BnB command-line interface."""
-    
+
     prompt = '(hbnb) '
-        
+
     def do_clear(self, args):
         """Clear the terminal.
         """
         print("\033c", end="")
-        
+
     def do_quit(self, args):
         """Quit command to exit the program. 
         """
         return True
-    
+
     def do_EOF(self, args):
         """Exit the command-line interface.
         """
-        return True    
+        return True
 
     def default(self, line):
         """Handle unknown commands.
@@ -29,7 +33,6 @@ class HBNBCommand(cmd.Cmd):
         """Print an empty line.
         """
         print(end='')
-
 
 
 if __name__ == '__main__':
