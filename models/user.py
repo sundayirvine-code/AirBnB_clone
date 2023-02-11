b883db706b1980ee22861de6592cd9ba6c44d2a8
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-11 20:40:32 +0300

    created decumentation for the user class

--- models/user.py
+++ models/user.py
@@ -1,10 +1,19 @@
 from models.base_model import BaseModel
 
 class User(BaseModel):
+    """
+    This class defines the User object and its attributes.
+    It inherits the `BaseModel` class and adds the email, password,
+    first_name, and last_name attributes.
+    """
     email = ''
     password = ''
     first_name = ''
     last_name = ''
 
     def __init__(self, *args, **kwargs):
+        """
+        Constructor for the User class
+        Calls the parent (BaseModel) class's constructor
+        """
         super().__init__(*args, **kwargs)
