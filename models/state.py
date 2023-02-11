cb924b31ce636a93faa95eacdf9598bddc1ee47d
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-11 20:44:36 +0300

    updated state class with documentation

--- models/state.py
+++ models/state.py
@@ -1,7 +1,18 @@
 from models.base_model import BaseModel
 
 class State(BaseModel):
+    """The State class inherits from BaseModel and represents a State of the application.
+
+    Attributes:
+        name (str): The name of the state.
+    """
     name = ''
 
     def __init__(self, *args, **kwargs):
+        """This method initializes the State class and calls its parent's __init__ method.
+
+        Args:
+            *args: Arguments passed to the parent's __init__ method.
+            **kwargs: Keyword arguments passed to the parent's __init__ method.
+        """
         super().__init__(*args, **kwargs)
