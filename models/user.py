9334fa3e6612c9e59a178efc75ce414d37587633
Ian Musima <sutomusima@gmail.com>
2023-02-11 18:57:12 +0000

    Fix pycodestyle errors and add docstring

--- models/user.py
+++ models/user.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+"""Defines the class User"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
+    """Represents a user and stores their personal details
+    Class attributes:
+        email (str): The user's email
+        password (str): The user's password
+        first_name (str): The user's first_name
+        last_name (str): The user's last_name
+    """
     email = ''
     password = ''
     first_name = ''
