570c348fc823c30d302baf261c66646cf5bd6ae3
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-11 21:05:37 +0300

    created documentation for the __init__ module

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-"""Init file"""
+"""
+Init file for the Models module
+"""
+
 from models.engine.file_storage import FileStorage
 
+# Instantiate the file storage engine
 storage = FileStorage()
+
+# Reload the objects from the file storage engine
 storage.reload()
