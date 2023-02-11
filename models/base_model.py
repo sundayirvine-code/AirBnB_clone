067ae4fac8663736b7cfb1ab8ff3e477b24fe936
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-11 21:00:31 +0300

    update BaseModel class with documentation

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,29 @@ from datetime import datetime
 
 
 class BaseModel:
-    """The parent class that defines all common attributes for other classes"""
+    """The parent class that defines all common attributes for other classes.
 
+    Attributes:
+        id (str): A unique identifier for the object.
+        created_at (datetime): The datetime the object was created.
+        updated_at (datetime): The datetime the object was last updated.
+
+    """
     def __init__(self, *args, **kwargs):
-        """Initializes the id, created_at, and updated_at attributes."""
+        """Initializes the id, created_at, and updated_at attributes.
+
+        If no kwargs are provided, the id will be set to a newly generated
+        UUID, created_at will be set to the current datetime, and updated_at
+        will be set to the same value as created_at. If kwargs are provided,
+        the id, created_at, and updated_at attributes will be set based on
+        the values passed in the kwargs.
+
+        Args:
+            *args: Variable length argument list. Not used.
+            **kwargs: Arbitrary keyword arguments. Can contain values for id,
+                created_at, and updated_at.
+
+        """
 
         if kwargs:
             for key, value in kwargs.items():
@@ -26,12 +45,24 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the object."""
+        """Returns a string representation of the object.
+
+        The string representation is in the format:
+        `[<Class Name>] (<id>) <object dictionary>`
+
+        Returns:
+            str: The string representation of the object.
+
+        """
 
         return f"[{self.__class__.__name__}] {(self.id)} {self.__dict__}"
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
+        """"Updates the updated_at attribute with the current datetime.
+
+        Also saves the object to the storage.
+
+        """
 
         now = datetime.now()
         self.updated_at = now
@@ -39,7 +70,16 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of the object."""
+        """Returns a dictionary representation of the object.
+
+        The dictionary contains key-value pairs for id, created_at,
+        updated_at, and class name. The created_at and updated_at values are
+        formatted as strings in the ISO format.
+
+        Returns:
+            dict: A dictionary representation of the object.
+
+        """
 
         my_dict = self.__dict__.copy()
         my_dict['updated_at'] = self.updated_at.isoformat()
