0830eec16f01fd2ece2eda7198ebafd2ade7ab89
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-11 21:03:26 +0300

    updated the amenity class with documentation

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,23 @@
 from models.base_model import BaseModel
 
 class Amenity(BaseModel):
+    """
+    Class Amenity defines the amenities provided by the Place
+    
+    Attributes:
+        name (str): the name of the Amenity
+    """
     name = ''
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes the Amenity class and its attributes.
+        
+        Args:
+            *args: variable length argument list
+            **kwargs: Arbitrary keyword arguments
+        
+        Returns:
+            None
+        """
         super().__init__(*args, **kwargs)
