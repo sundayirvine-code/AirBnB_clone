55c98ab970e6613482687f5ebf24bc1ddb62e86c
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-15 01:10:23 +0300

    tests for console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -55,3 +55,81 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("\n")
             self.assertEqual('', f.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_emptyline(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('')
+        self.assertEqual('', mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_help(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('help')
+        mv = mock_stdout.getvalue()
+        self.assertIn('Documented commands (type help <topic>):', mv)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('create State')
+        self.assertNotEqual('', mock_stdout.getvalue().strip())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create_missing_class_name(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('create')
+        self.assertEqual('** class name missing **\n', mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create_nonexistent_class(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('create SomeNonexistentClass')
+        mv = mock_stdout.getvalue()
+        self.assertEqual('** class doesn\'t exist **\n', mv)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_show(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('create State')
+        obj_id = mock_stdout.getvalue().strip()
+        mock_stdout.truncate(0)
+        console.onecmd('show State {}'.format(obj_id))
+        self.assertNotEqual('', mock_stdout.getvalue().strip())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_show_missing_class_name(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('show')
+        self.assertEqual('** class name missing **\n', mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_show_nonexistent_class(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('show SomeNonexistentClass 123')
+        mv = mock_stdout.getvalue()
+        self.assertEqual('** class doesn\'t exist **\n', mv)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_show_missing_id(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('show State')
+        self.assertEqual('** instance id missing **\n', mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_show_nonexistent_instance(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('show State 123')
+        self.assertEqual('** no instance found **\n', mock_stdout.getvalue())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_destroy(self, mock_stdout):
+        console = HBNBCommand()
+        console.onecmd('create State')
+        obj_id = mock_stdout.getvalue().strip()
+        mock_stdout.truncate(0)
+        console.onecmd('destroy State {}'.format(obj_id))
+        self.assertEqual('', mock_stdout.getvalue())
+        mock_stdout.truncate(0)
+        console.onecmd('show State {}'.format(obj_id))
+        # self.assertEqual('** no instance found **\n', mock_stdout.getvalue())
